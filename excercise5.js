9dbce374c4b1f688ff51694b9ec366059e443f7d
Tanmay Patel <tanmay@patelonline.net>
2024-05-11 16:18:14 +1200

    Excercise5

---
+++ excercise5.js
@@ -0,0 +1,18 @@
+let languages = ['Gujarati','Hindi','English','French','spanish'];
+console.log(languages);
+
+// Replace value of element 1 and 4
+languages[0]='Maori';
+languages[3]='Samoan';
+console.log(languages);
+
+//Add element at the end of the array.
+languages.push('Italian');
+console.log(languages);
+
+// remove last Element
+languages.pop();
+
+//reprint original Array
+console.log(languages);
+
