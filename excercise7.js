13cd5b7f55d5cc575780805b0e332d25228dc232
Tanmay Patel <tanmay@patelonline.net>
2024-05-11 17:58:37 +1200

    excercise7 with 3 loops based on operation parameter passed as argument to function

--- excercise7.js
+++ excercise7.js
@@ -1,6 +1,26 @@
 // Mathasmatical Operations Excercise 4.1
 
-function sum(num1, num2) {
+function operation(num1, num2, symbol) {
+  let operationtype = symbol;
+  if (operationtype == "+") {
+    let numsum = num1 + num2;
+    return numsum;
+  }
+  if (operationtype == "-") {
+    let numsum = num1 - num2;
+    return numsum;
+  }
+  if (operationtype == "/") {
+    let numsum = num1 / num2;
+    return numsum;
+  }
+  if (operationtype == "*") {
+    let numsum = num1 * num2;
+    return numsum;
+  }
+}
+
+/*function sum(num1, num2) {
   let numsum = num1 + num2;
   return numsum;
 }
@@ -16,32 +36,33 @@ function multiply(num1, num2) {
   let nummulty = num1 * num2;
   return nummulty;
 }
-
+*/
 // Console Output for functions
 
 // Calling funstions +, - , / and * respectively.
 
-for (let num1 = -1; num1 < 2; num1++) {
+for (let num1 = -1; num1 < 3; num1++) {
   let num2 = 4;
-  let sumtotal = sum(num1, num2);
-  console.log("Number 1 is = " + num1 + "," + "Numbner 2 = " + num2);
+  let sumtotal = operation(num1, num2, "+");
+  console.log("When Number1 is = " + num1 + " and " + "Numbner2 = " + num2);
+  console.log("");
   console.log(
-    "Should return = " + (num1 + num2) + " " + "Addition" + " ",
+    "Addition - " + "Should return = " + (num1 + num2) + " " +  " ",
     sumtotal == num1 + num2 ? "pass" : "fail"
   );
-  let subtotal = substract(num1, num2);
+let subtotal = operation(num1, num2,"-");
   console.log(
-    "Should return = " + (num1 - num2) + " " + "Substarction" + " ",
+    "Substarction - " + "Should return = " + (num1 - num2) + " " + " ",
     subtotal == num1 - num2 ? "pass" : "fail"
   );
-  let divtotal = division(num1, num2);
+  let divtotal = operation(num1, num2,"/");
   console.log(
-    "Should return = " + num1 / num2 + " " + "Division" + " ",
+    "Division - " +  "Should return = " + num1 / num2 + " " + " ",
     divtotal == num1 / num2 ? "pass" : "fail"
   );
-  let multiplytotal = multiply(num1, num2);
+  let multiplytotal = operation(num1, num2,"*");
   console.log(
-    "Should return = " + num1 * num2 + " " + "Multiplication" + " ",
+    "Multiplication - " + "Should return = " + num1 * num2 + " " + " ",
     multiplytotal == num1 * num2 ? "pass" : "fail"
   );
   console.log("");
