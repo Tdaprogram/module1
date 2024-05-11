b77650d439145ee8185fdaeac9e373b780a9f3b8
Tanmay Patel <tanmay@patelonline.net>
2024-05-11 13:18:54 +1200

    Mod1 Ex3

---
+++ functionmod1ex3.js
@@ -0,0 +1,11 @@
+function changeheader1() {
+    let itxtheader1 = document.getElementById("txtheader1").value;
+    document.getElementById("heading1").innerText = itxtheader1;       
+}
+
+function changeheader2() {
+    let itxtheader2 = document.getElementById("txtheader2").value;
+    document.getElementById("heading2").innerText = itxtheader2;
+    document.getElementById("btn_header2").innerText = "Header Changed";   
+}
+
