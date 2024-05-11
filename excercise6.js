53e2e638f13d8b760c5fb454090a5c45e24bf70f
Tanmay Patel <tanmay@patelonline.net>
2024-05-11 18:16:15 +1200

    excercise6.js

--- excercise6.js
+++ excercise6.js
@@ -5,16 +5,20 @@ let book = {
     genre: "Programming",
     pages: 15
 };
-
+console.log("");
+console.log("*****  Book Array Contents  *****")
+console.log("");
 console.log(book.title);
 console.log(book.genre);
 console.log(book.year);
 console.log(book.pages);
 console.log(book.author);
-//Originasl Book Data
-console.log(book);
+console.log("");
 
-///Change Title
+console.log('*****  With Original Description  *****');
+console.log(book);
+console.log("");
+console.log('*****  With Changed Description  *****');
 let newTitle = book.title
 newTitle = "Jason Object Variable"
 book.title = newTitle
