3c617ba419d99e22e21b5af166ad01b22e136216
Tanmay Patel <tanmay@patelonline.net>
2024-05-11 15:39:26 +1200

    Excercise4 part 1

---
+++ excercise4.js
@@ -0,0 +1,39 @@
+// Mathasmatical Operations Excercise 4.1
+
+function sum(num1,num2){
+    let numsum = num1 + num2;
+    return numsum
+}
+function substract(num1,num2){
+    let numsub = num1 - num2;
+    return numsub
+}
+function division(num1,num2){
+    let numdiv = num1  / num2;
+    return numdiv
+}
+function multiply(num1,num2){
+    let nummulty = num1  * num2;
+    return nummulty
+}
+
+// Console Output for functions
+
+// Addition
+
+let num1 = 20;
+let num2 = 10;
+
+// Calling funstions +, - , / and * respectively.
+
+let sumtotal = sum(num1,num2);
+console.log('Sum of numbers  = ' , + sumtotal);clearInterval
+
+let subtotal = substract(num1,num2);
+console.log('Substraction of numbers  = ' , + subtotal);clearInterval
+
+let divtotal = division(num1,num2);
+console.log('Division of numbers  = ' , + divtotal);clearInterval
+
+let multiplytotal = multiply(num1,num2);
+console.log('Sum of numbers  = ' , + multiplytotal);clearInterval
