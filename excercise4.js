1bdee185a1ead9e82c2315bec043de8b0cd060f2
Tanmay Patel <tanmay@patelonline.net>
2024-06-09 20:07:25 +1200

    Documented whole program

--- excercise4.js
+++ excercise4.js
@@ -1,26 +1,70 @@
-// Mathematical Operations Exercise 4.e
+/**
+ * This class represents a set of mathematical operations.
+ */
 class MathsOperations {
+  /**
+   * This function returns the full name of a student by concatenating the first name and last name.
+   *
+   * @param {string} firstName - The first name of the student.
+   * @param {string} lastName - The last name of the student.
+   * @returns {string} The full name of the student.
+   */
   studentName(firstName, lastName) {
     return firstName + " " + lastName;
   }
+
+  /**
+   * This function calculates the sum of two numbers.
+   *
+   * @param {number} num1 - The first number to add.
+   * @param {number} num2 - The second number to add.
+   * @returns {number} The sum of num1 and num2.
+   */
   sum(num1, num2) {
     let numsum = num1 + num2;
     return numsum;
   }
+
+  /**
+   * This function calculates the difference between two numbers.
+   *
+   * @param {number} num1 - The first number.
+   * @param {number} num2 - The second number.
+   * @returns {number} The difference between num1 and num2.
+   */
   subtract(num1, num2) {
     let numsub = num1 - num2;
     return numsub;
   }
+
+  /**
+   * This function calculates the division of two numbers.
+   *
+   * @param {number} num1 - The dividend.
+   * @param {number} num2 - The divisor.
+   * @returns {number} The result of dividing num1 by num2.
+   */
   division(num1, num2) {
     let numdiv = num1 / num2;
     return numdiv;
   }
+
+  /**
+   * This function calculates the product of two numbers.
+   *
+   * @param {number} num1 - The first number.
+   * @param {number} num2 - The second number.
+   * @returns {number} The product of num1 and num2.
+   */
   multiply(num1, num2) {
     let numMulty = num1 * num2;
     return numMulty;
   }
 }
 
+/**
+ * This script demonstrates the usage of the MathsOperations class.
+ */
 let mathsOperationsOutput = new MathsOperations();
 console.log(
   `Program written by  - ` +
@@ -30,61 +74,3 @@ console.log(mathsOperationsOutput.sum(20, 10));
 console.log(mathsOperationsOutput.subtract(20, 10));
 console.log(mathsOperationsOutput.division(20, 10));
 console.log(mathsOperationsOutput.multiply(20, 10));
-
-/*
-function sum(num1, num2) {
-  let numsum = num1 + num2;
-  return numsum;
-}
-function substract(num1, num2) {
-  let numsub = num1 - num2;
-  return numsub;
-}
-function division(num1, num2) {
-  let numdiv = num1 / num2;
-  return numdiv;
-}
-function multiply(num1, num2) {
-  let numMulty = num1 * num2;
-  return numMulty;
-}
-
-// Console Output for functions
-
-// Addition
-
-let num1 = 20;
-let num2 = 10;
-
-// Calling funstions +, - , / and * respectively.
-
-let sumtotal = sum(num1, num2);
-console.log("Sum of numbers  = ", +sumtotal);
-clearInterval;
-
-let subtotal = substract(num1, num2);
-console.log("Substraction of numbers  = ", +subtotal);
-clearInterval;
-
-let diagonal = division(num1, num2);
-console.log("Division of numbers  = ", +divtotal);
-clearInterval;
-
-let multiplytotal = multiply(num1, num2);
-console.log("Sum of numbers  = ", +multiplytotal);
-clearInterval;
-
-//Function to call name
-
-function fullNAme(firstName, lastName) {
-  return firstName + " " + lastName;
-}
-
-//Calling Function to print Full Name
-
-let fName = "Tanmay";
-let lName = "Patel";
-
-let personName = fullNAme(fName, lName);
-console.log(personName);
-*/
